dc0a4a10bdf79885f5db8a3e4451697a446f9e08
Ilya Shutikov <DeliasZen@gmail.com>
2022-01-09 19:36:02 +0300

    upgrade basket

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,19 @@
+import {
+  basketContainer,
+  eraseBasket,
+  createBasket,
+} from './shopping-basket.js';
+
 const contentContainer = document.querySelector('#content-container');
 const cartCounterLabel = document.querySelector('#cart-counter-label');
+const basketCartBtn = document.querySelector('.page-header__cart-btn');
 
 let cartCounter = 0;
 let cartPrice = 0;
+let productInBasketArr = null;
+let restoreHTML = null;
+
+const cartCounterLabelPrint = (c) => (c > 0) ? cartCounterLabel.innerHTML = '${c}' : cartCounterLabel.style.display = 'none';
 
 const incrementCounter = () => {
   cartCounterLabel.innerHTML = `${++cartCounter}`;
@@ -17,6 +28,8 @@ const getMockData = (t) => +t.parentElement
 
 const getPrice = (t, price) => Math.round((price + getMockData(t)) * 100) / 100;
 
+const getProductName = (t) => t.parentElement.parentElement.querySelector('.item-title').innerHTML;
+
 const disabledControls = (t, fn) => {
   t.disabled = true;
   contentContainer.removeEventListener('click', fn);
@@ -26,17 +39,48 @@ const enableControls = (t, fn) => {
   contentContainer.addEventListener('click', fn);
 };
 
+const writeProductToBasket = (t, arr) => {
+  let item = null;
+
+  let product = {
+    productName: getProductName(t),
+    productCode: getProductName(t),
+    price: getMockData(t),
+    count: 1,
+    sum: getMockData(t)
+  };
+  if (arr !== null) {
+    let i = 0;
+
+    while ((item === null) && (i < arr.length)) {
+      (arr[i].productCode === getProductName(t)) ? item = i : i++
+    };
+    if (item === null) arr.push(product)
+    else {
+      arr[item].count++;
+      arr[item].sum = Math.round((arr[item].sum + arr[item].price) * 100) / 100;
+    };
+
+  } else arr = [product];
+
+  return arr;
+};
+
 const btnClickHandler = (e) => {
   const target = e.target;
-  const interval = 2000;
+  const interval = 1000;
   let restoreHTML = null;
 
   if (target && target.matches('.item-actions__cart')) {
+    if (basketContainer !== null) eraseBasket();
+
     incrementCounter();
+
+    productInBasketArr = writeProductToBasket(target, productInBasketArr);
+
     restoreHTML = target.innerHTML;
     cartPrice = getPrice(target, cartPrice);
     target.innerHTML = `Added ${cartPrice.toFixed(2)} $`;
-
     disabledControls(target, btnClickHandler)
 
     setTimeout(() => {
@@ -46,4 +90,58 @@ const btnClickHandler = (e) => {
   };
 };
 
-contentContainer.addEventListener('click', btnClickHandler)
+const basketBtnHandler = (e) => {
+  const target = e.target;
+
+  if (target) {
+    eraseBasket();
+    if (!target.classList.contains('basket__btn-next')) {
+      cartPrice = 0;
+      productInBasketArr = null;
+      cartCounter = 0;
+      cartCounterLabelPrint(cartCounter);
+    };
+  };
+};
+
+const createBasketWork = (c, pArr) => {
+  createBasket(c, pArr);
+  const basketBtn = document.querySelector('#btn-container');
+  basketBtn.addEventListener('click', basketBtnHandler);
+  basketContainer.addEventListener('click', delProductHandler);
+};
+
+const delProductHandler = (e) => {
+  const target = e.target;
+
+  if (target && target.matches('basket__item-del')) {
+    let b = true;
+    let i = 0;
+
+    while (b && i < productInBasketArr.length) {
+      if (productInBasketArr[i].productCode === target.parentElement.dataset.code) {
+        cartPrice -= productInBasketArr[i].sum;
+        cartCounter -= productInBasketArr[i].count;
+
+        productInBasketArr.splice(i, 1);
+        b = false;
+      } else i++;
+    };
+    eraseBasket();
+    createBasketWork(cartCounter, productInBasketArr);
+    cartCounterLabelPrint(cartCounter);
+  };
+};
+
+const basketClickHandler = (e) => {
+  const target = e.target;
+  if (target) {
+    createBasketWork(cartCounter, productInBasketArr);
+  };
+};
+
+export const shopInit = () => {
+  contentContainer.addEventListener('click', btnClickHandler);
+  basketCartBtn.addEventListener('click', basketClickHandler)
+};
+// contentContainer.addEventListener('click', btnClickHandler)
